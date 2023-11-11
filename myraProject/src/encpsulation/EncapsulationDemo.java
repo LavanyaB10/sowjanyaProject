558bae49534cd928216795af7495c56af4beb427
LavanyaB10 <149042265+LavanyaB10@users.noreply.github.com>
2023-11-11 14:30:29 +0530

    added encapsulation file

---
+++ myraProject/src/encpsulation/EncapsulationDemo.java
@@ -0,0 +1,26 @@
+package encpsulation;
+
+public class EncapsulationDemo {
+	String name;
+	int age;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+		
+		
+	}
+		
+	
+
+
